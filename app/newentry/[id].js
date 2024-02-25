33bdc1a80bc1676f883808b238d700ab4aa3a12b
Ayush <pandeyayushraj4@gmail.com>
2024-02-25 19:56:19 +0000

    Started work on the entry page

--- app/newentry/[id].js
+++ app/newentry/[id].js
@@ -1,59 +1,40 @@
 import React, { useState } from 'react';
-import { View, StyleSheet, Text, TextInput, Button } from 'react-native';
+import {COLORS} from '../../constants/theme';
+import {useRouter, Stack } from 'expo-router';
+import {SafeAreaView, Text, TextInput, View } from 'react-native';
+import ScreenHeaderBtn from '../../components/common/header/ScreenHeaderBtn';
+import icons from '../../constants/icons';
+import styles from './[id]Styles';
 
-const QuestionBox = () => {
-  const [title, setTitle] = useState(''); // State variable for the title
-  const [content, setContent] = useState(''); // State variable for the content in the colored box
-  const [entry, setEntry] = useState(''); // State variable for the entry in the big text input box
+const newEmotionEntry = () => {
+  const [title, setTitle] = useState(''); 
+  const [content, setContent] = useState(''); 
+  const [entry, setEntry] = useState(''); 
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>{title}</Text>
-      <TextInput
-        style={styles.coloredBox}
-        placeholder="Real-Time Questions (AI)"
-        onChangeText={(text) => setContent(text)}
-        value={content}
-      />
-      <TextInput
-        style={styles.entryBox}
-        placeholder="Describe what you're feeling and get Al generated real-time questions to help you cope."
-        onChangeText={(text) => setEntry(text)}
-        value={entry}
-        multiline={true} // Allow multiple lines of text
-      />
-      <View style={styles.buttonRow}>
-        <Button title="Button 1" onPress={() => { /* Handle button press here */ }} />
-        <Button title="Button 2" onPress={() => { /* Handle button press here */ }} />
-        <Button title="Button 3" onPress={() => { /* Handle button press here */ }} />
-      </View>
-    </View>
-  );
+    <SafeAreaView style={{flex: 1, backgroundColor: COLORS.background }}>
+
+            <Stack.Screen
+            options={{
+                headerStyle: {backgroundColor: COLORS.background},
+                headerLeft: () => (
+                    <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
+                )
+            }}/>
+
+            <View style={styles.container}>
+               <Text style={styles.title}>Title</Text>
+                   <TextInput
+                      style={styles.coloredBox}
+                      placeholder="Real-Time Questions (AI)"
+                      onChangeText={(text) => setContent(text)}
+                      value={content}
+                    />
+            </View>
+
+    </SafeAreaView>
+  )
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 20,
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-  },
-  coloredBox: {
-    backgroundColor: '#f2f2f2',
-    padding: 10,
-    borderRadius: 5,
-    marginBottom: 10,
-  },
-  entryBox: {
-    padding: 10,
-    marginBottom: 10,
-  },
-  buttonRow: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-  },
-});
 
-export default QuestionBox;
+export default newEmotionEntry;
---
+++ app/newentry/[id]Styles.js
@@ -0,0 +1,19 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      padding: 20,
+    },
+    title: {
+      fontSize: 24,
+      fontWeight: 'bold',
+    },
+    coloredBox: {
+      backgroundColor: '#f2f2f2',
+      padding: 10,
+      borderRadius: 5,
+      marginBottom: 10,
+    },
+})
+export default styles;
