4447d601e7d93d5e429a93f37ffb7ae880eca1c4
Ayush <pandeyayushraj4@gmail.com>
2024-02-27 18:52:12 +0000

    fixed entry page without scroll

--- app/newentry/[id].js
+++ app/newentry/[id].js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 import {COLORS} from '../../constants/theme';
 import {useRouter, Stack } from 'expo-router';
-import {SafeAreaView, Text, TextInput, View } from 'react-native';
+import {Dimensions, SafeAreaView, Text, TextInput, View } from 'react-native';
 import ScreenHeaderBtn from '../../components/common/header/ScreenHeaderBtn';
 import icons from '../../constants/icons';
 import styles from './[id]Styles';
+import { LinearGradient } from 'expo-linear-gradient';
 
 const newEmotionEntry = () => {
   const [title, setTitle] = useState(''); 
@@ -23,13 +24,28 @@ const newEmotionEntry = () => {
             }}/>
 
             <View style={styles.container}>
-               <Text style={styles.title}>Title</Text>
-                   <TextInput
-                      style={styles.coloredBox}
-                      placeholder="Real-Time Questions (AI)"
-                      onChangeText={(text) => setContent(text)}
-                      value={content}
-                    />
+              <TextInput
+                style={styles.inputBoxTitle}
+                placeholder="Title"
+                placeholderTextColor="rgba(0, 0, 0, 0.5)"
+              />
+
+              <TextInput
+                style={styles.bodyInputBox}
+                placeholder="Describe what you’re feeling and get AI generated real-time questions to help you cope. "
+                placeholderTextColor="rgba(0, 0, 0, 0.5)"
+                multiline
+                numberOfLines={Math.floor(Dimensions.get('window').height / 4 / 20)} // Adjust the height as needed
+              />
+
+              <TextInput
+                style={styles.inputBoxNormal}
+                placeholder="You can journal anything you don’t want AI to know here."
+                placeholderTextColor="rgba(0, 0, 0, 0.5)"
+                multiline
+                onChangeText={(text) => setContent(text)}
+                value={content}
+              />
             </View>
 
     </SafeAreaView>
--- app/newentry/[id]Styles.js
+++ app/newentry/[id]Styles.js
@@ -1,16 +1,46 @@
-import { StyleSheet } from "react-native";
+import { Dimensions, StyleSheet } from "react-native";
+import { COLORS } from "../../constants/theme";
+import { LinearGradient } from 'expo-linear-gradient';
 
 const styles = StyleSheet.create({
     container: {
       flex: 1,
       padding: 20,
     },
+    inputBoxTitle: {
+      backgroundColor: 'transparent',
+      fontSize: 26,
+      fontWeight: 'bold',
+      color: 'rgba(0, 0, 0, 0.7)',
+      borderBottomColor: 'black', // Adjust the color as needed
+      paddingVertical: 10,
+    },
+    bodyInputBox: {
+      backgroundColor: COLORS.background,
+      fontSize: 16,
+      color: 'rgba(0, 0, 0, 0.7)',
+      padding: 10,
+      borderWidth: 1,
+      height: Dimensions.get('window').height / 4,
+      borderColor: 'transparent',
+      borderTopColor: '#00FFD1',
+      borderRightColor: '#00CCE7',
+      borderBottomColor: '#0094FF',
+      borderLeftColor: '#00CCE7',
+      borderRadius: 20,
+    },
+    inputBoxNormal: {
+      backgroundColor: 'transparent',
+      fontSize: 16,
+      color: 'rgba(0, 0, 0, 0.7)',
+      paddingVertical: 10,
+    },
     title: {
       fontSize: 24,
       fontWeight: 'bold',
     },
     coloredBox: {
-      backgroundColor: '#f2f2f2',
+      backgroundColor: COLORS.background,
       padding: 10,
       borderRadius: 5,
       marginBottom: 10,
